50f5cb0d2cc9d3f03a5f81c3749d7fcf5fafd83c
R KOUSHIK VARMA <koushikvarma6369@gmail.com>
2025-03-29 03:58:06 +0530

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -9,7 +9,7 @@ function About() {
 
   useEffect(() => {
     const checkMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
+      setIsMobile(window.innerWidth <= 768); // Check if the screen width is <= 768px for mobile
     };
 
     // Initial check
@@ -22,22 +22,22 @@ function About() {
   }, []);
 
   useEffect(() => {
-    console.log('Element is in view: ', isInView);
+    console.log('Element is in view: ', isInView); // Log when the element is in view
   }, [isInView]);
 
   const mobileText = {
     intro:
-      'Hello! I am R Koushik varma, a Full Stack Developer passionate about DevOps and AI ML. I hold a Bachelor of Engineering from Sathyabama Institute of science and Technology.',
-    experience:
-      '',
+      'Hello! I am R Koushik varma, a Full Stack Developer passionate about DevOps and AI ML. I hold a Bachelor of Engineering from Sathyabama Institute of Science and Technology.',
+    experience: '',
   };
 
   const desktopText = {
     intro:
-      'Hello! I am R Koushik varma, a Full Stack Developer passionate about DevOps and AI ML. With experience across a wide array of technologies, I strive to deliver scalable, cost-effective solutions. I hold a Bachelor of Technology from Sathyabama Institute of science and Technology, with a focus on Frontend development, backend development, and intelligent automation.',
+      'Hello! I am R Koushik varma, a Full Stack Developer passionate about DevOps and AI ML. With experience across a wide array of technologies, I strive to deliver scalable, cost-effective solutions. I hold a Bachelor of Technology from Sathyabama Institute of Science and Technology, with a focus on Frontend development, backend development, and intelligent automation.',
     experience:
-      'As a dedicated student passionate about web development, I have honed my skills through a combination of personal projects, academic coursework, and online learning. I’ve developed and deployed several full-stack web applications, utilizing technologies such as HTML, CSS, JavaScript, React, and Node.js.',
-      'My projects range from simple static websites to more dynamic applications involving APIs and real-time data integration.I’ve gained practical experience by collaborating with fellow students on group projects and contributing to open-source communities. I am constantly improving my technical abilities, and I am eager to apply my skills to real-world problems, bringing fresh perspectives and a strong willingness to learn',
+      'As a dedicated student passionate about web development, I have honed my skills through a combination of personal projects, academic coursework, and online learning. I’ve developed and deployed several full-stack web applications, utilizing technologies such as HTML, CSS, JavaScript, React, and Node.js. ' +
+      'My projects range from simple static websites to more dynamic applications involving APIs and real-time data integration. I’ve gained practical experience by collaborating with fellow students on group projects and contributing to open-source communities. ' +
+      'I am constantly improving my technical abilities, and I am eager to apply my skills to real-world problems, bringing fresh perspectives and a strong willingness to learn.',
   };
 
   return (
@@ -77,7 +77,6 @@ function About() {
             <li className="about-grid-info-list-item">Express</li>
             <li className="about-grid-info-list-item">Node.js</li>
             <li className="about-grid-info-list-item">TailwindCSS</li>
-
           </ul>
         </div>
         <div className="about-grid-photo">
